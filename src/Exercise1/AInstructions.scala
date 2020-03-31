5e092c52b3ee7de40c56eb751f1dd1eace91989c
mickeygoldmeier <45402517+mickeygoldmeier@users.noreply.github.com>
2020-03-31 14:50:39 +0300

    add the add,sub,gt,eq,pop,neg functions to convert from vm to asm

--- src/Exercise1/AInstructions.scala
+++ src/Exercise1/AInstructions.scala
@@ -1,6 +1,155 @@
 package Exercise1
 
 object AInstructions {
+  ///  pop
+  def pop(segment: String, offset: Int, fileName: String): String = {
+    // function for function-level segments: local / argument / this / that
+    def functionLevel(segment: String, offset: Int): String = {
+      var pop = "@" + offset + "\n" // load the offset into A
+      pop += "D=A\n" // move the offset into D
+      pop += "@" + segment + "\n" // load the address of segment into A
+      pop += "D=M+D\n" // calculate the segment + offset into D
+      pop += "@13\n" // load the temporary into A
+      pop += "M=D\n" // save the data of segment + offset into the temp room
+      pop += "@SP\n" // load the SP into A
+      pop += "M=M-1\n" // decrease the sp by 1
+      pop += "A=M\n" // load the pointer of the next free room
+      pop += "D=M\n" // save the data of segment + offset into the free room
+      pop += "@13\n" // load the SP into A
+      pop += "A=M\n" // load the pointer of the next free room
+      pop += "M=D\n" // save the data of segment + offset into the free room
+
+      // return the result to the main program
+      pop
+    }
+
+    // function for pointer / temp segments
+    def pointerTempLevel(segment: String, offset: Int): String = {
+      var pop = "@" + offset + "\n" // load the offset into A
+      pop += "D=A\n" // store the offset in D
+      if (segment.equals("pointer"))
+        pop += "@3\n" // load the pointer value (3) into A
+      else
+      pop += "@5\n" // load the temp value (5) into A
+      pop += "D=A+D\n" // calculate the value of segment + offset
+      pop += "@13\n" // load SP into A
+      pop += "M=D\n" // get the next free room pointer into A
+      pop += "@SP\n" // load the SP into A
+      pop += "M=M-1\n" // move the pointer to the next free room
+      pop += "A=M\n" // get the next free room pointer into A
+      pop += "D=M\n" // load the SP into A
+      pop += "@13\n" // load SP into A
+      pop += "A=M\n" // get the next free room pointer into A
+      pop += "M=D\n" // get the next free room pointer into A
+
+      // return the result to the main program
+      pop
+    }
+
+    // function for static segments
+    def staticLevel(fileName: String, offset: Int): String = {
+      var pop = "@SP\n" // load SP into A
+      pop += "M=M-1\n" // get the next free room pointer into A
+      pop += "A=M\n" // store the data of fileName.offset from D into the free room
+      pop += "D=M\n" // store the data of fileName.offset from D into the free room
+      pop += "@" + fileName + "." + offset + "\n" // load the pointer to fileName.offset into A
+      pop += "M=D\n" // store the value of fileName.offset into D
+
+      // return the result to the main program
+      pop
+    }
+
+    def constantLevel(value: Int): String = {
+      var pop = "@sp\n" // load the value into A
+      pop += "M=M-1\n" // put the value from A into D
+
+      // return the result to the main program
+      pop
+    }
+
+    // return the result of the right segment type
+    if (segment.equals("local") || segment.equals("argument") || segment.equals("this") || segment.equals("that"))
+      return functionLevel(segment, offset)
+    if (segment.equals("pointer") || segment.equals("temp"))
+      return pointerTempLevel(segment, offset)
+    if (segment.equals("static"))
+      return staticLevel(fileName, offset)
+    constantLevel(offset)
+  }
+
+  def gt(instruction: Int): String = {
+    var gt = "@SP\n" // get the location of the first empty place in the stack (SP)
+    gt += "M=M-1\n" // change to the location of the 1st value in the stack and thats where it needs to be at the end
+    gt += "A=M\n" // get the loction of the 1st value
+    gt += "D=M\n" // get the value of the 1st
+    gt += "A=A-1\n" // get the location of the 2nd value
+    gt += "D=D-M\n" // subtract the 2nd from the 1st
+    gt += "@GT_" + instruction + "\n" // load the location of the code if GT is true
+    gt += "D;JGT\n" // jump to the label if X is really GT then Y
+    gt += "@NOT_GT_" + instruction + "\n" // load the location of the code if GT is false
+    gt += "D=0;JEQ\n" // load FALSE into D and jump to the false loction
+    gt += "(GT_" + instruction + ")\n" // set the label for GT
+    gt += "D=-1\n" // load TRUE into D
+    gt += "(NOT_GT_" + instruction + ")\n" // set the label for NOT GT
+    gt += "M=D\n" // put the result in the 2nd place
+
+    // return the result to the main program
+    gt
+  }
+
+  def eq(instruction: Int): String = {
+    var eq = "@sp\n" //A=0 the location of the stack pointer
+    eq += "M=M-1\n" //decrease the stack pointer by 1 and thats where it needs to be at the end
+    eq += "A=M\n" //dump the loction the sp is pointing at to A
+    eq += "D=M\n" //dump into D the first value
+    eq += "A=A-1\n" //get the loction of the second value
+    eq += "D=M-D\n" //dump into D the value of V1-V2
+    eq += "@EQ_" + instruction + "\n" // load the location of the code if EQ is true
+    eq += "D;JEQ" //if eq is true jump to the loction in A
+    eq += "@NOT_EQ_" + instruction + "\n" // load the location of the code if EQ is false
+    eq += "D=0;JEQ\n" //load FALSE into D and jump to the false loction
+    eq += "(EQ_" + instruction + ")\n" // set the label for EQ
+    eq += "D=-1\n" //loat true to D
+    eq += "(NOT_EQ_" + instruction + ")\n" // set the label for NOT_EQ
+    eq += "M=D\n" //load the result into the stack
+
+    // return the result to the main program
+    eq
+  }
+
+  def neg(): String = {
+    var neg = "@sp\n" //A=0 the location of the stack pointer
+    neg += "A=M-1\n"    //dump the loction the sp is pointing at to A
+    neg += "D=M\n"    //get the value into D
+    neg += "M=-D\n"  //dump into the same location the value of -V
+
+    // return the result to the main program
+    neg
+  }
+
+  def sub(): String = {
+    var sub = "@sp\n" //A=0 the location of the stack pointer
+    sub += "M=M-1\n"  //decrease the stack pointer by 1 and thats where it needs to be at the end
+    sub += "A=M\n"    //dump the loction the sp is pointing at to A
+    sub += "D=M\n"    //get the second value into D
+    sub += "A=A-1\n"  //get the loction of the first value
+    sub += "M=M-D\n"  //dump into the same location the value of V1-V2
+
+    // return the result to the main program
+    sub
+  }
+
+  def add(): String = {
+    var add = "@sp\n" //A=0 the location of the stack pointer
+    add += "M=M-1\n"  //decrease the stack pointer by 1 and thats where it needs to be at the end
+    add += "A=M\n"    //dump the loction the sp is pointing at to A
+    add += "D=M\n"    //get the first value into D
+    add += "A=A-1\n"  //get the loction of the second value
+    add += "M=D+M\n"  //dump into the same location the value of V1+V2
+
+    // return the result to the main program
+    add
+  }
 
   def lt(instruction: Int): String = {
     var lt = "@SP\n" // get the location of the first empty place in the stack (SP)
@@ -98,7 +247,9 @@ object AInstructions {
       push += "M=D\n" // save the data of segment + offset into the free room
       push += "@SP\n" // load the SP into A
       push += "M=M+1\n" // move the pointer to the next free room
-      push // return the result to the main program
+
+      // return the result to the main program
+      push
     }
 
     // function for pointer / temp segments
